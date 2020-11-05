5a7c278c8acc1286bf83862531675e0a14963aa6
david <davidrguardiola@hotmail.es>
2020-11-05 14:04:10 +0100

    Merge origin/master
    Conflicts:
    	Manual/Manual/src/Ejemplo_Entornos_Graficos/Ejemplo_Lamina_Texto.java

--- src/textodelimitado/TextoDelimitado.java
+++ src/textodelimitado/TextoDelimitado.java
@@ -13,6 +13,7 @@ import java.io.FileWriter;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.NumberFormat;
 import productsstream.Producto;
 
 /**
@@ -26,10 +27,10 @@ public class TextoDelimitado {
      */
     public static void main(String[] args) {
 
-       Producto producto1 = new Producto();
-        String[] cod = {"p1","p2","p3"};
-        String[] desc = {"parafusos","cravos","tachas"};
-        Double[] prezo = {3.0,4.0,5.0};
+        Producto producto1 = new Producto();
+        String[] cod = {"p1", "p2", "p3"};
+        String[] desc = {"parafusos", "cravos", "tachas"};
+        Double[] prezo = {3.0, 4.0, 5.0};
 
         try {
 
@@ -37,29 +38,45 @@ public class TextoDelimitado {
 
             BufferedReader ler = new BufferedReader(new FileReader(new File("C:/Users/david/Desktop/prueba clase file/productos1.txt")));
 
-            for (int i = 0; i <cod.length; i++) {
+            for (int i = 0; i < cod.length; i++) {
 
-                escribir.println(cod[i]+"\t"+desc[i]+"\t"+prezo[i]+"\t");
-               
-                
+                escribir.println(cod[i] + "\t" + desc[i] + "\t" + prezo[i]);
+
             }
 
             escribir.close();
-            
-          /*  for (int j=0;j<cod.length;j++){
-            
-                System.out.println("Codigo: " +ler.readLine());
+
+            String cadea = ler.readLine();
+            String[] valor = cadea.split("\t");
+
            
+
+            Producto po3 = new Producto();
+
+            po3.setCodigo(valor[0]);
+            po3.setDescricion(valor[1]);
+            po3.setPrezo(Double.parseDouble(valor[2]));
+
+            System.out.println(po3.toString());
+
+            cadea = ler.readLine();
+            valor = cadea.split("\t");
+
+            po3.setCodigo(valor[0]);
+            po3.setDescricion(valor[1]);
+            po3.setPrezo(Double.parseDouble(valor[2]));
             
-            }*/
-            
-            System.out.println(ler.readLine());
-            System.out.println(ler.readLine());
-            System.out.println(ler.readLine());
-            
-            
-            
-            
+
+            System.out.println(po3.toString());
+
+            cadea = ler.readLine();
+            valor = cadea.split("\t");
+
+            po3.setCodigo(valor[0]);
+            po3.setDescricion(valor[1]);
+            po3.setPrezo(Double.parseDouble(valor[2]));
+
+            System.out.println(po3.toString());
 
         } catch (IOException ex) {
 
